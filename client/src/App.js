86d6b395325f19b14d74155e737255a610091335
HuiGong-dev <hellohuigong@gmail.com>
2023-01-02 15:07:01 +0100

    init

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@mui/material";
+
+const App = () => {
+  return (
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Memories
+        </Typography>
+      </AppBar>
+    </Container>
+  );
+};
+
+export default App;
