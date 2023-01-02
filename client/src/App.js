4d4753a601823f3311875299be2f4ac6112fae18
HuiGong-dev <hellohuigong@gmail.com>
2023-01-02 19:14:17 +0100

    wip

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,36 @@
 import React from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@mui/material";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+import useStyles from "./styles";
 
 const App = () => {
+  const styles = useStyles();
   return (
     <Container maxWidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">
+      <AppBar position="static" color="inherit" sx={styles.appBar}>
+        <Typography variant="h2" align="center" sx={styles.heading}>
           Memories
         </Typography>
       </AppBar>
+
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing="3"
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
     </Container>
   );
 };
